43be2d501bcd24268b294fd4a5d6e729e6ed71bd
Ameer Hamza <r3alst@gmail.com>
2023-02-09 22:30:12 +0500

    Migration to onecademy prod

--- extensions/chrome/src/lib/firebase.ts
+++ extensions/chrome/src/lib/firebase.ts
@@ -7,8 +7,7 @@ const app = initializeApp({
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  appId: process.env.FIREBASE_APP_ID
 });
 
 const db = getFirestore(app);
