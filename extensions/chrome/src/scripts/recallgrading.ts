9c763d97ac26599462343100bb979e3a30173daa
Ameer Hamza <r3alst@gmail.com>
2023-03-15 05:09:16 +0500

    Prompt Updated

--- extensions/chrome/src/scripts/recallgrading.ts
+++ extensions/chrome/src/scripts/recallgrading.ts
@@ -487,6 +487,7 @@ export const recallGradingBot = async (gptTabId: number, prevRecallGrade?: Query
       phraseLines.join("\n") + "\n" +
       `Your response should include a section for each of the above listed key phrases, separated by ---- enclosed by two new line characters.\n` +
       `Each section of your response, which corresponds to one of the above listed key phrases, should include the following three lines:\n` +
+      `- DO NOT write the key phrase in your response. We understand your answers based on the order of key phrases.\n` +
       `- In the first line, only print YES or NO. Do not add any more explanations. DO NOT write any line before this line.\n` +
       `- In the second line of your response, only print a percentage by which you are confident about your answer, YES or NO, in the first line.\n` +
       `- In the third line of your response, explain why you answered YES or NO in the first line and the percentage in the second line.`;
