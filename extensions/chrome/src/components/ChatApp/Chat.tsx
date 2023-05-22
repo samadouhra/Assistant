262be03e5ad8284d53aecd33854b8d39dd7e073a
elVengador <dev.jimynicanor@gmail.com>
2023-05-22 09:52:30 -0500

    fix default value of notebook

--- extensions/chrome/src/components/ChatApp/Chat.tsx
+++ extensions/chrome/src/components/ChatApp/Chat.tsx
@@ -121,7 +121,7 @@ type ChatProps = {
 
 export const Chat = ({ isLoading, setIsLoading, appMessages, clearAppMessages, isAuthenticated, sx }: ChatProps) => {
 
-  const [notebook, setNotebook] = useState<Notebook | null>({ id: "123", name: "fake notebook, remove it" })
+  const [notebook, setNotebook] = useState<Notebook | null>(null)
   const [messagesObj, setMessagesObj] = useState<Message[]>([]);
   const [speakingMessageId, setSpeakingMessageId] = useState<string>("");
   const chatElementRef = useRef<HTMLDivElement | null>(null);
@@ -395,7 +395,7 @@ export const Chat = ({ isLoading, setIsLoading, appMessages, clearAppMessages, i
           // console.log(1)
           pushMessage(generateTopicNotFound(request ?? "", isAuthenticated), getCurrentDateYYMMDD())
         } else {
-          // console.log(22)
+          console.log(22, notebook)
           onPushAssistantMessage({ ...(message as IAssistantResponse), nodes: [] })
           const nodesOnMessage = nodes ? nodes.map(mapNodesToNodeLink) : []
           if (!nodesOnMessage.length) return setIsLoading(false);
@@ -408,6 +408,7 @@ export const Chat = ({ isLoading, setIsLoading, appMessages, clearAppMessages, i
             setIsLoading(false);
             return
           }
+          console.log(222, { isAuthenticated })
           if (!isAuthenticated) return setIsLoading(false);
           const payload: IAssistantCreateNotebookRequestPayload = { conversationId, message: request }
           chrome.runtime.sendMessage(chrome.runtime.id || process.env.EXTENSION_ID, {
